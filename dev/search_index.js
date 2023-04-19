eb707132bb57b1fcf8ba02bffb916e8d987527ed
Documenter.jl <documenter@juliadocs.github.io>
2023-04-19 12:36:10 +0000

    build based on abfbf5f

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = ChatTools","category":"page"},{"location":"#ChatTools","page":"Home","title":"ChatTools","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for ChatTools.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [ChatTools]","category":"page"}]
+}
